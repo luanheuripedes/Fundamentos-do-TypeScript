d5bd8e4884214cbed4b5e7e01a60c4e13dcefeb3
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2023-01-09 19:29:46 -0300

    Arrays

---
+++ Tipos de dados basicos/3 - Arrays/dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+//Arrays Forma de declarar
+let notas = [];
+let paises;
+//Declarar atribuindo valores
+let cidades = [
+    'Manhuaçu',
+    'Reduto',
+    'Minas Gerais'
+];
+//Adicionar item array
+notas.push(10);
+
+//Acessar valor array
+console.log(notas[0]);
+
+
---
+++ Tipos de dados basicos/3 - Arrays/src/index.ts
@@ -0,0 +1,21 @@
+//Arrays Forma de declarar
+let notas: number[] = [];
+let paises: Array<string>;
+
+
+//Declarar atribuindo valores
+let cidades =[
+    'Manhuaçu',
+    'Reduto',
+    'Minas Gerais'
+];
+
+
+//Adicionar item array
+notas.push(10);
+
+//Acessar valor array
+console.log(notas[0]);
+
+//Comprimento do vetor
+console.log(cidades.length);
