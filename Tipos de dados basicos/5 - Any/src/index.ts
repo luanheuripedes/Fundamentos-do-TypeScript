96164ce77a37c1d9ca3d00c464cf0f1d98d7c697
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2023-01-09 19:44:10 -0300

    Any

--- Tipos de dados basicos/5 - Any/src/index.ts
+++ Tipos de dados basicos/5 - Any/src/index.ts
@@ -1,36 +1,16 @@
-let cliente = {
-    nome:'Luan',
-    email:'luanheuripedesmoreiradepaula',
-    saldo:100,
-    ativo:true
-}
+let nome:any;
 
-cliente = {
-    nome:'Carols',
-    email:'Carols',
-    saldo:100,
-    ativo:true
-}
+nome = 'Luan';
+nome = 1;
 
-//propriedades complexas
-let produto = {
-    codigo:1,
-    descricao:'Arroz',
-    categoria:{
-        codigo:10,
-        descricao:'Cereais'
-    }
-}
+nome = {
+    idade:19
+};
 
 
-let aluno:{
-    matricula:string,
-    nome: string
-}
+// Any em função
+nome = () =>{
+    console.log("ola")
+};
 
-aluno = {
-    matricula:'001',
-    nome:'Luan'
-}
-console.log(cliente);
-console.log(produto);
+nome();
