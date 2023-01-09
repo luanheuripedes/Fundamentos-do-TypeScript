22e289d840da1cc09a040746076dda84c1bbd181
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2023-01-09 18:40:38 -0300

    Iniciando os tipos basicos

---
+++ Tipos de dados basicos/1 - String,Number e boolean/dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+//string
+let nome = 'Joel';
+let email = "Teste@gmail.com";
+console.log(typeof nome);
+//number
+let idade = 29;
+let temperatura = 36.7;
+console.log(typeof idade);
+console.log(typeof temperatura);
+//boolean
+let verdadeiro = true;
+let falso = false;
+console.log(typeof verdadeiro);
+console.log(typeof falso);
+//Concatenar strings
+console.log('A' + 'B');
+console.log('A' + 'B' + 3);
+console.log(4 * 3);
