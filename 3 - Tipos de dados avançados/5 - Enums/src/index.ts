f84ffcbb6dd74b76bf83ba76a1c466e5d7ef0d7f
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2023-01-11 13:56:17 -0300

    Enums

--- 3 - Tipos de dados avançados/5 - Enums/src/index.ts
+++ 3 - Tipos de dados avançados/5 - Enums/src/index.ts
@@ -1,25 +1,62 @@
-/*
-function exibirMensagem(){
-    console.log('Ola!');
+//type TipoPersonagem = "Mago" | "Elfo";
+
+enum TipoPersonagem{
+    Mago = 100,
+    Elfo = 'Elfo'
 }
 
-exibirMensagem();
-*/
+abstract class Personagem{
+    nome: string;
+    tipo: TipoPersonagem;
 
-// Parametros de entrada
-/*
-function exibirMensagem(mensagem: string, nome:string){
-    console.log(mensagem + nome);
+    constructor(nome:string, tipo: TipoPersonagem){
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+
+    caminhar(){
+        console.log(`${this.nome} Caminhou`);
+    }
+
+    abstract atacar(): void;
+
 }
 
-exibirMensagem('Parametro de entrada ', 'Luan');
-*/
+class Mago extends Personagem{
 
-// Retorno das funções
-function calcularNovoPreco(precoOriginal: number, taxaReajuste:number):number{
-    let novoPreco = precoOriginal * (1+taxaReajuste/100);
-    return novoPreco;
+    constructor(nome:string){
+        super(nome,TipoPersonagem.Mago);
+    }
+
+    caminhar(){
+        console.log(`${this.nome} voou`);
+    }
+
+    atacar(): void {
+        console.log(`${this.nome} atacou com magia`);
+    }
 }
 
-let precoAjustado = calcularNovoPreco(100,-50);
-console.log(precoAjustado);
+class Elfo extends Personagem{
+    
+
+    constructor(nome:string){
+        super(nome,TipoPersonagem.Elfo);
+    }
+
+    atacar(): void {
+        console.log(`${this.nome} atacou com arco e flecha`);
+    }
+}
+
+let p1 = new Mago('Saruman');
+let p2 = new Elfo('Elrond');
+
+console.log(p1);
+console.log(p2);
+
+p1.caminhar();
+p2.caminhar();
+
+p1.atacar();
+p2.atacar();
