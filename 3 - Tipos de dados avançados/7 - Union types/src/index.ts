7c48da11f6014f4e5dfda54292b6defe81495b73
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2023-01-11 14:08:24 -0300

    Union Type

--- 3 - Tipos de dados avançados/7 - Union types/src/index.ts
+++ 3 - Tipos de dados avançados/7 - Union types/src/index.ts
@@ -1,25 +1,8 @@
-/*
-function exibirMensagem(){
-    console.log('Ola!');
-}
+let nome: string | null|number;
 
-exibirMensagem();
-*/
+nome = 'Luan';
 
-// Parametros de entrada
-/*
-function exibirMensagem(mensagem: string, nome:string){
-    console.log(mensagem + nome);
-}
+nome = null;
 
-exibirMensagem('Parametro de entrada ', 'Luan');
-*/
+nome = 0;
 
-// Retorno das funções
-function calcularNovoPreco(precoOriginal: number, taxaReajuste:number):number{
-    let novoPreco = precoOriginal * (1+taxaReajuste/100);
-    return novoPreco;
-}
-
-let precoAjustado = calcularNovoPreco(100,-50);
-console.log(precoAjustado);
